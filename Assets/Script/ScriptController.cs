91a1f58dbe875169c052abaf91e8df0313180413
Charlesss7 <90251876+Charlesss7@users.noreply.github.com>
2021-09-15 18:56:25 +0700

    Change

---
+++ Assets/Script/ScriptController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptController : MonoBehaviour
+{
+    private int currentScore = 0;
+
+    private void Start()
+    {
+        // reset
+        currentScore = 0;
+    }
+
+    public float GetCurrentScore()
+    {
+        return currentScore;
+    }
+
+    public void IncreaseCurrentScore(int increment)
+    {
+        currentScore += increment;
+    }
+
+    public void FinishScoring()
+    {
+        // set high score
+        if (currentScore > ScoreData.highScore)
+        {
+            ScoreData.highScore = currentScore;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
